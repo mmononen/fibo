1ce979458f2bd53030f50f9e65ca5359ae840fbf
mmononen <10145483+mmononen@users.noreply.github.com>
2023-10-14 21:44:10 +0300

    Add Kotlin implementation

---
+++ src/fibo.kt
@@ -0,0 +1,15 @@
+fun fibonacci(x: Int): Int {
+    if (x > 1) {
+        return fibonacci(x - 1) + fibonacci(x - 2)
+    } else {
+        return x
+    }
+}
+
+fun main(args : Array<String>) {
+    var iValue = 45
+
+    println("Fibonacci Sequence Tool")
+    println("Copyright (c) 2023 Mikko Mononen")
+    println("The Fibonacci number for $iValue is $fibonacci(iValue).")
+}
